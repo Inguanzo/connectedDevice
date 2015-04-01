1d14c9c1fb569322e53ab885ee7067540c5f5383
Inguanzo <Inguanzo24@berkeley.edu>
2015-03-31 20:25:16 -0700

    app working function 1

--- connectedApp/src/main.js
+++ connectedApp/src/main.js
@@ -5,7 +5,7 @@ var BUTTONS = require("controls/buttons");
 deviceURL = "";
 
 var whiteSkin = new Skin( { fill:"white" } );
-var labelStyle = new Style( { font: "bold 40px", color:"black" } );
+var labelStyle = new Style( { font: "bold 15px", color:"black" } );
 
 
 Handler.bind("/discover", Behavior({
@@ -24,22 +24,71 @@ Handler.bind("/forget", Behavior({
 	}
 }));
 
-//var counterLabel = new Label({left:0, right:0, height:40, string:"refill", style: labelStyle});
-var ResetButton = BUTTONS.Button.template(function($){ return{
-	left: 80, right: 80, height:60,
+var foodCounterLabel = new Label({left:75, top:-40, height:40, string:"null", style: labelStyle});
+var waterCounterLabel = new Label({right:30, top:-40, height:40, string:"null", style: labelStyle});
+
+
+var RefillFoodButton = BUTTONS.Button.template(function($){ return{
+	left: 5, right: 250, top:0, height:30,
 	contents: [
 		new Label({left:0, right:0, height:60, string:"Refill", style: labelStyle})
 	],
 	behavior: Object.create(BUTTONS.ButtonBehavior.prototype, {
 		onTap: { value: function(content){
-			content.invoke(new Message(deviceURL + "reset"), Message.JSON);
+			content.invoke(new Message(deviceURL + "foodRefill"), Message.JSON);
 		}},
 		onComplete: { value: function(content, message, json){
-			//counterLabel.string = "full";//json.count;
+			foodCounterLabel.string = json.val;
 		}}
 	})
 }});
 
+var RefreshFoodButton = BUTTONS.Button.template(function($){ return{
+	left: 75, right: 180, top: -30, height:30,
+	contents: [
+		new Label({left:0, right:0, height:60, string:"Refresh", style: labelStyle})
+	],
+	behavior: Object.create(BUTTONS.ButtonBehavior.prototype, {
+		onTap: { value: function(content){
+			content.invoke(new Message(deviceURL + "foodRefresh"), Message.JSON);
+		}},
+		onComplete: { value: function(content, message, json){
+			foodCounterLabel.string = json.foodCount;
+		}}
+	})
+}});
+
+var RefillWaterButton = BUTTONS.Button.template(function($){ return{
+	right: 70, left: 190, top: -30, height:30,
+	contents: [
+		new Label({left:0, right:0, height:60, string:"Refill", style: labelStyle})
+	],
+	behavior: Object.create(BUTTONS.ButtonBehavior.prototype, {
+		onTap: { value: function(content){
+			content.invoke(new Message(deviceURL + "waterRefill"), Message.JSON);
+		}},
+		onComplete: { value: function(content, message, json){
+			waterCounterLabel.string = json.waterRefillVal;
+		}}
+	})
+}});
+
+var RefreshWaterButton = BUTTONS.Button.template(function($){ return{
+	right: 5, left: 255, top: -30, height:30,
+	contents: [
+		new Label({left:0, right:0, height:60, string:"Refresh", style: labelStyle})
+	],
+	behavior: Object.create(BUTTONS.ButtonBehavior.prototype, {
+		onTap: { value: function(content){
+			content.invoke(new Message(deviceURL + "waterRefresh"), Message.JSON);
+		}},
+		onComplete: { value: function(content, message, json){
+			waterCounterLabel.string = json.waterRefreshVal;
+		}}
+	})
+}});
+
+/*
 var ResetWaterButton = BUTTONS.Button.template(function($){ return{
 	left: 80, right: 80, height:60, bottom: 20,
 	contents: [
@@ -50,27 +99,29 @@ var ResetWaterButton = BUTTONS.Button.template(function($){ return{
 			content.invoke(new Message(deviceURL + "resetWater"), Message.JSON);
 		}},
 		onComplete: { value: function(content, message, json){
-			//counterLabel.string = "full";//json.count;
+			//foodCounterLabel.string = json.count;
 		}}
 	})
 }});
-
+*/
 var mainColumn = new Column({
 	left: 0, right: 0, top: 0, bottom: 0, active: false, skin: whiteSkin,
 	contents: [
-		new Label({left:0, right:0, height:40, string:"Food:", style: labelStyle}),
-		//counterLabel,
-		new ResetButton(),
-		new Label({left:0, right:0, height:40, bottom: 20, string:"Water:", style: labelStyle}),
-		//counterLabel,
-		new ResetWaterButton()
+		new Label({left:10, height:40, top:10, string:"Food:", style: labelStyle}),
+	 	new Label({right:70, height:40, top: -40, string:"Water:", style: labelStyle}),
+		foodCounterLabel,
+		waterCounterLabel,
+		new RefillFoodButton(),
+		new RefreshFoodButton(),
+		new RefillWaterButton(),
+		new RefreshWaterButton(),
 	],
 	behavior: Behavior({
 		onTouchEnded: function(content){
 			if (deviceURL != "") content.invoke(new Message(deviceURL + "getCount"), Message.JSON);
 		},
 		onComplete: function(content, message, json){
-			//counterLabel.string = json.count;
+			foodCounterLabel.string = json.count;
 		}	
 	})
 });
@@ -84,5 +135,27 @@ var ApplicationBehavior = Behavior.template({
 	},
 })
 
+/*
+Handler.bind("/getTime", {
+    onInvoke: function(handler, message){
+        handler.invoke(new Message(deviceURL + "/getCount"), Message.JSON);
+    },
+    onComplete: function(handler, message, json){
+         //mainColumn.timeLabel.string = json.time;
+         trace("******inside app");
+         handler.invoke( new Message("/delay"));
+    }
+});
+
+Handler.bind("/delay", {
+    onInvoke: function(handler, message){
+        handler.wait(10000); //will call onComplete after 10 seconds
+    },
+    onComplete: function(handler, message){
+        handler.invoke(new Message("/getTime"));
+    }
+});
+*/
 application.behavior = new ApplicationBehavior();
 application.add(mainColumn);
+//application.invoke(new Message("/getTime"));
